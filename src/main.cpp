a7d9fc81347595d3cbb74b74650c11f513e36338
mars12938 <77018143+mars12938@users.noreply.github.com>
2024-12-28 21:06:24 -0500

    forked sfml cmake repo

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,22 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({500, 500}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
